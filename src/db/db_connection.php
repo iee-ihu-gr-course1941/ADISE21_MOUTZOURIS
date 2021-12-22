5325f8d2f076a85cc7de1d7d64039a3bcb70fea6
Kerv2310 <kerverospateras@gmail.com>
2021-12-22 19:07:15 +0200

    DB connection

---
+++ src/db/db_connection.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost';
+$db = 'moutzouris';
+require_once "db_password.php";
+
+$user = $DB_USER;
+$pass = $DB_PASS;
+
+if (gethostname() == 'users.iee.ihu.gr') {
+    $conn = new mysqli($host, $user, $pass, $db, null, '/home/student/it/2017/it174916/mysql/run/mysql.sock');
+} else {
+    $conn = new mysqli($host, $user, '', $db);
+}
+
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: (" .
+        $conn->connect_errno . ") " . $conn->connect_error;
+}
---
+++ src/db/db_password.php
@@ -0,0 +1,4 @@
+<?php
+	$DB_PASS = 'miltosTzouvaras28!';
+	$DB_USER = 'root';
+?>
