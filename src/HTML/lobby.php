3e21f1760a74aaab62551d2a01c5ed77ade2f965
Kerv2310 <kerverospateras@gmail.com>
2022-01-05 15:36:33 +0200

    Update lobby.php

--- src/HTML/lobby.php
+++ src/HTML/lobby.php
@@ -8,3 +8,38 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
         exit();
     }
 ?>
+
+
+<!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Lobby</title>
+        <link rel="stylesheet" href="../css/lobby.css">
+        <link rel="icon" type ="text/css" href="../../assets/poker_chip.png">
+    </head>
+
+    <body>
+        <div class="top">
+            <div class='current-user'>
+                <h2 class="username">Χρήστης: <?php echo $_SESSION['username']; ?></h2>
+                <button onclick="location.href = '../auth/logout.php';" id="logout-button" class="logout-button">Αποσύνδεση</button>
+            </div>
+        </div>
+        <h1>Lobby</h1>
+        <div class="table">
+            <table id="lobby">
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                </tr>
+            </table>
+        </div>
+        <p id='info'>2-4 παίκτες για την έναρξη του παιχνιδιού</p>
+        <button id="start" class='disabled' onclick="startGame()" disabled> Έναρξη </button>
+                  
+    </body>
+    <script src="../scripts/lobby.js"></script>
+
+    </html>
