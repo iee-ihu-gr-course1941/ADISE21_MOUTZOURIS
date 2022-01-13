0ecbffce6dbd37d85064e99a614e0f2fbf1d2e8d
Miltos Tzouvaras <miltostzouvaras99@gmail.com>
2022-01-13 18:39:14 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -44,7 +44,7 @@ CREATE TABLE lobby (
 
 INSERT INTO cards VALUES ('1','C',NULL),('2','C',NULL),('3','C',NULL),('4','C',NULL),('5','C',NULL),('6','C',NULL),('7','C',NULL),('8','C',NULL),('9','C',NULL),('10','C',NULL),('13','C',NULL),('1','D',NULL),('2','D',NULL),('3','D',NULL),('4','D',NULL),('5','D',NULL),('6','D',NULL),('7','D',NULL),('8','D',NULL),('9','D',NULL),('10','D',NULL),('1','H',NULL),('2','H',NULL),('3','H',NULL),('4','H',NULL),('5','H',NULL),('6','H',NULL),('7','H',NULL),('8','H',NULL),('9','H',NULL),('10','H',NULL),('1','S',NULL),('2','S',NULL),('3','S',NULL),('4','S',NULL),('5','S',NULL),('6','S',NULL),('7','S',NULL),('8','S',NULL),('9','S',NULL),('10','S',NULL);
 
-INSERT INTO users VALUES ('Alex','2310'),('Miltos','lolo');
+INSERT INTO users VALUES (default,'Alex','2310'),(default,'Miltos','lolo');
 
 
 
