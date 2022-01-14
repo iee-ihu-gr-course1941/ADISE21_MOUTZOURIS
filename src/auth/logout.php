3293fe610e4ca89b12929593255b658539997d6c
Kerv2310 <kerverospateras@gmail.com>
2022-01-14 12:58:10 +0200

    Update logout.php

--- src/auth/logout.php
+++ src/auth/logout.php
@@ -2,10 +2,11 @@
 session_start();
 include "../db/db_connection.php";
 
-unset($_SESSION["user_id"]);
-unset($_SESSION["username"]);
-$sql = "DELETE FROM lobby WHERE pid='$user_id'";
+$uid = $_SESSION['user_id'];
+$sql = "DELETE FROM lobby WHERE pid='$uid'";
 mysqli_query($conn,$sql);
+unset($_SESSION['user_id']);
+unset($_SESSION['username']);
 header("Location: ../HTML/index.php");
 
 ?>
