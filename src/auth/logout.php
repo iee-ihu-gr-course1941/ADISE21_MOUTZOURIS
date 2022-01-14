3b486764e27e9584b63822a49f2f12ed2322eb4b
Miltos Tzouvaras <miltostzouvaras99@gmail.com>
2022-01-14 12:52:39 +0200

    Update logout.php

--- src/auth/logout.php
+++ src/auth/logout.php
@@ -6,6 +6,6 @@ unset($_SESSION["user_id"]);
 unset($_SESSION["username"]);
 $sql = "DELETE FROM lobby WHERE pid='$user_id'";
 mysqli_query($conn,$sql);
-header("Location: index.php");
+header("Location: ../HTML/index.php");
 
 ?>
